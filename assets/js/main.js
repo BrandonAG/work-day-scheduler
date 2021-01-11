ef208915d7afcaaed1eec7ad26da90417fa0dba3
Brandon Arnst-Goodrich <brandon.eg@gmail.com>
2021-01-10 19:04:16 -0800

    added saving and retrieval from local storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,72 @@
+var savedSchedule = {};
+
+// Grab existing schedule from local storage
+function checkLocalStorage() {
+    if (localStorage.getItem("todolist") === null) {
+        savedSchedule = {};
+    }
+    else {
+        savedSchedule = JSON.parse(localStorage.getItem("todolist"));
+        console.log(savedSchedule)
+    }
+}
+
+// Update current date and display in header
 function displayDate() {
     $("#currentDay").text(moment().format("dddd, MMMM D, YYYY"));
 }
 
+// Populate the days work schedule
 function updateSchedule() {
     for (var h = 9; h <= 17; h++) {
-        console.log(h);
         $(".container").append('<div class="row"></div>');
-        $(".row").last().append('<div class="col-1 hour">' + moment(h, "HH").format("h a") + '</div>');
-        $(".row").last().append('<textarea class="col-10 description future">Enter Value</textarea>');
-        $(".row").last().append('<button class="col-1 saveBtn"><i class="fas fa-save"></i></button>');
-        // $(".saveBtn").last().append('<i class="fas fa-save"></i>');
+        var hourContent = $('<div>');
+        hourContent.addClass("col-1 hour");
+        hourContent.text(moment(h, "HH").format("h a"));
+        $(".row").last().append(hourContent);
+        var descriptionContent = $('<textarea>');
+        var due;
+        if (moment().format("HH") > h) {
+            due = "past";
+        }
+        else if (moment().format(HH) < h) {
+            due = "future";
+        }
+        else {
+            due = "present";
+        }
+        descriptionContent.addClass("col-10 description " + due);
+        descriptionContent.attr("task-hour", h);
+        if (h in savedSchedule) {
+            descriptionContent.text(savedSchedule[h]);
+        }
+        $(".row").last().append(descriptionContent);
+        var saveContent = $('<button>');
+        saveContent.addClass("col-1 saveBtn");
+        saveContent.attr("save-hour", h);
+        saveContent.html('<i class="fas fa-save"></i>');
+        $(".row").last().append(saveContent);
     }
 }
 
+// Save edited task descriptions to local storage
+function saveSchedule() {
+    $(document).on('click', '.saveBtn', function() {
+        var hourNumber = $(this).attr('save-hour');
+        var taskDescription = $('[task-hour="' + hourNumber + '"]').val();
+        if (taskDescription === "") {
+            delete savedSchedule[hourNumber.toString()];
+        }
+        else {
+            savedSchedule[hourNumber.toString()] = taskDescription;
+        }
+        console.log(savedSchedule);
+        console.log(JSON.stringify(savedSchedule))
+        localStorage.setItem('todolist', JSON.stringify(savedSchedule));
+    });
+}
+
 displayDate()
+checkLocalStorage()
 updateSchedule()
+saveSchedule()
